2c248ec1e8c12eecca577b3aa1ce1e657f8e9041
hrytsiukpavlo <overdrive800392@gmail.com>
2022-02-16 20:53:40 +0200

    Finished application
    Finished application

---
+++ index.js
@@ -0,0 +1,37 @@
+import FetchWrapper from "./fetch-wrapper.js"
+
+const API = new FetchWrapper("https://v6.exchangerate-api.com/v6/82fdb027659aadf1f807ca57")
+
+const base = document.querySelector("#base-currency");
+const result = document.querySelector("#conversion-result");
+const input = document.querySelector('#input-number');
+
+
+const getConversionRates = () => {
+    if (!input.value) {
+        input.value = 1;
+    }
+    API.get(`/latest/${base.value}`)
+    .then(data => {
+        if (input.value) {
+            result.textContent = (data.conversion_rates['UAH'] * input.value).toFixed(2);
+        } else {
+            result.textContent = (data.conversion_rates['UAH']).toFixed(2);
+        }
+    });
+}
+
+const inputChangeHandler = (event) =>{
+    if (!base.value){
+        alert('Оберіть валюту')
+    } else {
+        API 
+            .get(`/latest/${base.value}`)
+            .then(data => {
+                result.textContent = (data.conversion_rates['UAH'] * event.target.value).toFixed(2);
+            });
+    }
+}
+
+base.addEventListener('change', getConversionRates);
+input.addEventListener('change', inputChangeHandler);
